bddea3040235523616df11051b5ee574896116dd
SunilDas21 <105918661+SunilDas21@users.noreply.github.com>
2024-07-14 01:20:04 +0530

    Add files via upload

---
+++ InstaaaaSafe/src/test/java/Mohs10/autoit_runner/AppTest.java
@@ -0,0 +1,20 @@
+package Mohs10.autoit_runner;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
